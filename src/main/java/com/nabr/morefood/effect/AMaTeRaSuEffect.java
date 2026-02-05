173e2d0a69ddd9f5b1fdbc1d75be1c4c08e8278a
NaBr406 <xhn2223008825@outlook.com>
2026-02-06 00:24:08 +0800

    新增药水效果

--- src/main/java/com/nabr/morefood/effect/AMaTeRaSuEffect.java
+++ src/main/java/com/nabr/morefood/effect/AMaTeRaSuEffect.java
@@ -4,7 +4,11 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Box;
+
+import java.util.List;
 
 public class AMaTeRaSuEffect extends StatusEffect {
 	public AMaTeRaSuEffect() {
@@ -18,6 +22,12 @@ public class AMaTeRaSuEffect extends StatusEffect {
     public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
         if (entity.hasStatusEffect(StatusEffects.DARKNESS)){
             entity.removeStatusEffect(StatusEffects.DARKNESS);
+        }
+        Box box = entity.getBoundingBox().expand(5.0);
+        List<LivingEntity> targets = world.getEntitiesByClass(LivingEntity.class , box , e -> e != entity);
+
+        for (LivingEntity target : targets) {
+            target.setOnFireFor(5);
         }
         return true;
     }
